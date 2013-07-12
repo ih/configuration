de7912435f1c52e3dadf0b9a1a004fd893e8b2f4
ih <irvin.hwang@gmail.com>
2013-07-12 14:11:14 -0700

    less files start css mode

--- emacs/.emacs.d/settings/css-settings.el
+++ emacs/.emacs.d/settings/css-settings.el
@@ -1,5 +1,7 @@
 (provide 'css-settings)
 
+(setq auto-mode-alist (cons '("\\.less$" . css-mode) auto-mode-alist))
+
 (add-hook 'css-mode-hook'(lambda ()
 		       (setq tab-width 2)
 		       (setq indent-tabs-mode t)))
